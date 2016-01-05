c1c89edd532bb6d59d4ce5cdc0fe88edff64b03b
Dang Vu Duy <Dev@dangs-macbook-air.local>
2016-01-05 14:23:55 +0700

    Initial Commit

---
+++ MyFunnyFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFunnyFood
+//
+//  Created by Dang Vu Duy on 1/5/16.
+//  Copyright © 2016 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
